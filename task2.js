8fe84523bd7556e4be887e12c60c5681cffddc8a
faymoons <127589178+faymoons@users.noreply.github.com>
2026-04-22 22:13:06 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,1 +1,6 @@
-
+let students = ["Phainon", "Cyrene", "Aglaea", "Anaxagoras", "Hyacine"];
+for (let i = 0; i < students.length; i++) {
+  console.log(students[i].toUpperCase());
+}
+students.push("Khaslana");
+console.log(students);
