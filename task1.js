014761acde6a04697251866cee04be53b526f79e
faymoons <127589178+faymoons@users.noreply.github.com>
2026-04-22 22:09:35 +0300

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,5 @@
+let name = "Yeva";
+const age = 17;
+console.log(`Hi, my name is ${name} and I am ${age} years old.`);
+name = "Ivory";
+console.log(`Hi, my name is ${name} and I am ${age} years old.`);
